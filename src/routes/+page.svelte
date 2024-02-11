d2d58150bafe8c92d0f9a1436e672c717d804bb3
Gustavo Rodriguez <gusdev@protonmail.com>
2024-02-11 15:09:41 -0500

    Main page layout fix.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,30 @@
 <h1 class="text-4xl font-bold my-8">Choose a test</h1>
 
-<div class="columns-md pt-2">
-	<a href="/hiragana"
-		 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-		Hiragana
-	</a>
-	<a href="/katakana"
-		 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-		Katakana
-	</a>
-	<a href="/romanji"
-		 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-		Romanji
-	</a>
-	<a href="/read"
-		 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-		Reading exercise
-	</a>
+<div class="columns sm:columns-sm md:columns-md pt-2">
+	<div class="mb-4 inline-block">
+		<a href="/hiragana"
+			 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+			Hiragana
+		</a>
+	</div>
+	<div class="mb-4 inline-block">
+		<a href="/katakana"
+			 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+			Katakana
+		</a>
+	</div>
+	<div class="mb-4 inline-block">
+		<a href="/romanji"
+			 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+			Romanji
+		</a>
+	</div>
+	<div class="mb-4 inline-block">
+		<a href="/read"
+			 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+			Reading exercise
+		</a>
+	</div>
 </div>
 
 <style>
